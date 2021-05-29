f94915b821c475649e037285f719f23ba171168d
JNiii <43835501+JNiii@users.noreply.github.com>
2021-05-29 15:56:08 +0800

    Add files via upload
    Clock Timer

---
+++ Clock_Timer.py
@@ -0,0 +1,28 @@
+import time
+
+def get_user_input():
+    sec = int(input('Enter time in seconds: '))
+    return sec
+
+def count_down(seconds):
+    while seconds != 0:
+        minute, sec = seconds // 60, seconds % 60
+        time.sleep(1)
+        print(minute,'min', sec,'sec')
+        seconds -= 1
+    print('Time is up!')
+
+def stop_watch():
+    seconds = 0
+    try:
+        while True:
+            minute, sec = seconds // 60, seconds % 60
+            time.sleep(1)
+            print(minute, 'min', sec, 'sec')
+            seconds += 1
+    except:
+        print('Time taken before stopping:', minute, 'min', sec, 'sec')
+
+
+
+ 
